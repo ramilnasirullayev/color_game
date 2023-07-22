f64c0a8a55df9267edd38fa47365f9f8c9f7ca3a
Ramil Nasirullayev <ramilnasirullayev@Ramils-MacBook-Pro.local>
2023-07-22 11:29:23 +0400

    adding game mode functionality

--- main.py
+++ main.py
@@ -3,9 +3,8 @@ from src.game import *
 from src.gameDisplay import *
 
 
-
 if __name__ == "__main__":
-    board = Board(size=18, colors=['r', 'b', 'g', 'y'])
+    board = Board(size=18, colors=['red', 'blue', 'green', 'yellow'], mode='easy')
     game = Game(board=board, max_turns=21)
     display = GameDisplay(game=game)
     display.play_game()
--- src/gameDisplay.py
+++ src/gameDisplay.py
@@ -1,31 +1,35 @@
-from colorama import init, Fore, Style
+import os
+from termcolor import colored
+from game import *
 
 class GameDisplay:
-    def __init__(self, game):
+    def __init__(self, game: Game):
         self.game = game
         self.color_map = {
-            'r': Fore.RED,
-            'b': Fore.BLUE,
-            'g': Fore.GREEN,
-            'y': Fore.YELLOW,
+            'red': 'red',
+            'blue': 'blue',
+            'green': 'green',
+            'yellow': 'yellow',
+            # Add more colors here if necessary.
+            # The keys should match the colors used in your game.
         }
-        init(autoreset=True)  # Initialize colorama
 
     def print_board(self):
         for row in self.game.board.board:
             for color in row:
-                print(f'{Style.BRIGHT}██{self.color_map[color]}██', end=' ')
+                print(colored('██', self.color_map[color]), end=' ')
             print()
 
     def play_game(self):
         print('Game Start!')
         while not self.game.has_ended():
+            os.system('cls' if os.name == 'nt' else 'clear')  # Clears the console
             self.print_board()
-            color_choice = input(f"Turn {self.game.turns + 1}. Choose a color from {', '.join(self.game.board.colors)}: ").strip().lower()
+            color_choice = input(f"Turn {self.game.turns + 1}. Choose a color from {self.game.board.colors}: ").strip().lower()
             while color_choice not in self.game.board.colors:
-                color_choice = input(f"Invalid color! Choose a color from {', '.join(self.game.board.colors)}: ").strip().lower()
+                color_choice = input(f"Invalid color! Choose a color from {self.game.board.colors}: ").strip().lower()
             self.game.play_turn(color_choice)
             if self.game.is_game_won():
-                print(Fore.GREEN + "Congratulations, you've won!")
+                print("Congratulations, you've won!")
                 return
-        print(Fore.RED + "Game Over. You've lost.")
+        print("Game Over. You've lost.")
