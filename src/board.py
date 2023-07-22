a3e3555eac2c3ca3df1668751ddce1017f42d230
Ramil Nasirullayev <ramilnasirullayev@Ramils-MacBook-Pro.local>
2023-07-22 10:27:51 +0400

    adding dfs to fill

--- src/board.py
+++ src/board.py
@@ -10,15 +10,14 @@ class Board:
         board = [[random.choice(self.colors) for _ in range(self.size)] for _ in range(self.size)]
         return board
 
-    def update_board(self, new_color):
-        start_color = self.board[0][0]
-        visited = [[False for _ in range(self.size)] for _ in range(self.size)]
-        queue = [(0, 0)]
-        while queue:
-            i, j = queue.pop(0)
-            if (i < 0 or i >= self.size or j < 0 or j >= self.size or
-                    visited[i][j] or self.board[i][j] != start_color):
-                continue
-            self.board[i][j] = new_color
-            visited[i][j] = True
-            queue.extend([(i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)])
+    def dfs_fill(self, i, j, start_color, new_color, visited):
+        if i < 0 or i >= self.size or j < 0 or j >= self.size or visited[i][j] or self.board[i][j] != start_color:
+            return
+
+        visited[i][j] = True
+        self.board[i][j] = new_color
+
+        self.dfs_fill(i - 1, j, start_color, new_color, visited)  # Up
+        self.dfs_fill(i + 1, j, start_color, new_color, visited)  # Down
+        self.dfs_fill(i, j - 1, start_color, new_color, visited)  # Left
+        self.dfs_fill(i, j + 1, start_color, new_color, visited)  # Right
